b88af6b5584b1602c9fe591b95f48caa5b816224
sn77sohan <sohan.ahmedself@gmail.com>
2021-11-07 19:37:43 +0600

    second-edition

--- src/App.js
+++ src/App.js
@@ -38,3 +38,100 @@ function Product(props) {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   const products =[
+//     {name: 'mobile', price:150000},
+//     {name: 'camera', price:55000},
+//     {name: 'laptop', price:17000},
+//     {name: 'watch', price:5000},
+//     {name: 'shoe', price:5000},
+    
+//   ]
+//   return (
+//     <div className="App">
+
+//       {
+//         products.map(product => <Product name={product.name} price={product.price}></Product>)
+//       }
+     {/* <Product name="mobile" price="15000"></Product>
+     <Product name="Computer" price="700000"></Product>
+     <Product name="Hand watch" price="5000"></Product>
+     <Product name="Laptop" price="170000"></Product> */}
+     
+    {/* </div>
+  );
+} */}
+// function Product(props) {
+//   const productStyle = {
+//     border: '2px solid yellow',
+//     borderRadius: '10px'
+//   }
+//   return (
+//     <div className="product" style={productStyle}>
+//       <h2>Name: {props.name}</h2>
+//       <h4>Price: {props.price}</h4>
+//     </div>
+//   )
+// }
+
+// export default App;
